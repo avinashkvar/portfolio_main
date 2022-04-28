71b1d8c8172af5684040dd4aefc3b15d461e0c26
avinashkvar <98077343+avinashkvar@users.noreply.github.com>
2022-04-28 11:20:26 +0530

    change

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -13,6 +13,16 @@ import Col from 'react-bootstrap/esm/Col';
 import Card from 'react-bootstrap/esm/Card';
 import Modal from 'react-bootstrap/Modal';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+	faCss3Alt,
+	faDocker,
+	faHtml5,
+	faJs,
+	faReact,
+	faNode,
+} from '@fortawesome/free-brands-svg-icons';
+
 const Experience = ({ darkMode }) => {
 	const [show, setShow] = useState(false);
 
@@ -24,91 +34,133 @@ const Experience = ({ darkMode }) => {
 			<br></br>
 			<Fade>
 				<Container>
-						<br></br>
-						<h2 className="lead" align="center">
-							<b>- Projects -</b>
-						</h2>
-						<br></br>
-						<Zoom>
-							<Row>
-								<Col lg={4} sm={12}>
-									<Card
-										align="center"
-										className="exp"
-										data-theme={darkMode ? 'dark' : 'light'}
-									>
-										<br></br>
-										<center>
-											<div class="flip-card">
-												<div class="flip-card-inner">
-													<div className="flip-card-front">
-														<Card.Img variant="top" src={tcd} />
-														<Card.Body>
-															<Card.Title align="center">
-																Freshly.com(Clone)
-															</Card.Title>
-															<Card.Text>
-																<p align="center">
-																	A Web Application where user
-																	can Order food.
-																</p>
-															</Card.Text>
-														</Card.Body>
-													</div>
-													<div className="flip-card-back">
-														<p>
-															A Food Ordering Web Application
-															where users can login to the site
-															and add their favourite dishes to
-															the cart and User can also sort and
-															filter his favorite foods and pay
-															the payment and the dishes get
-															delivered to their Residentail Home
-															Address provided in the site.
-														</p>
-													</div>
+					<br></br>
+					<h2 className="lead" align="center">
+						<b>- Projects -</b>
+					</h2>
+					<br></br>
+					<Zoom>
+						<Row>
+							<Col lg={4} sm={12}>
+								<Card
+									align="center"
+									className="exp"
+									data-theme={darkMode ? 'dark' : 'light'}
+								>
+									<br></br>
+									<center>
+										<div class="flip-card">
+											<div class="flip-card-inner">
+												<div className="flip-card-front">
+													<Card.Img variant="top" src={tcd} />
+													<Card.Body>
+														<Card.Title align="center">
+															Freshly.com(Clone)
+														</Card.Title>
+														<Card.Text>
+															<p align="center">
+																A Web Application where user can
+																Order food.
+															</p>
+														</Card.Text>
+													</Card.Body>
+												</div>
+												<div className="flip-card-back">
+													<p>
+														A Food Ordering Web Application where
+														users can login to the site and add
+														their favourite dishes to the cart and
+														User can also sort and filter his
+														favorite foods and pay the payment and
+														the dishes get delivered to their
+														Residentail Home Address provided in
+														the site.
+													</p>
+													<Button
+														variant={
+															darkMode
+																? 'outline-light'
+																: 'outline-dark'
+														}
+														className="skills-btn"
+													>
+														<FontAwesomeIcon
+															className="skill"
+															size="2x"
+															icon={faHtml5}
+														/>
+													</Button>
+													<Button
+														variant={
+															darkMode
+																? 'outline-light'
+																: 'outline-dark'
+														}
+														className="skills-btn"
+													>
+														<FontAwesomeIcon
+															className="skill"
+															size="2x"
+															icon={faCss3Alt}
+														/>
+													</Button>
+													<Button
+														variant={
+															darkMode
+																? 'outline-light'
+																: 'outline-dark'
+														}
+														className="skills-btn"
+													>
+														<FontAwesomeIcon
+															className="skill"
+															size="2x"
+															icon={faJs}
+														/>
+													</Button>
 												</div>
 											</div>
-										</center>
-										<br></br>
-										<div>
-											<a
-												href="https://clonefreshly.netlify.app/"
-												target="_blank"
-												rel="noreferrer noopener"
-											>
-												<Button
-													variant={
-														darkMode
-															? 'outline-light'
-															: 'outline-dark'
-													}
-													className="sbtn"
-												>
-													View Project
-												</Button>
-											</a>
-											<a
-												href="https://github.com/avinashkvar/freshly.git"
-												target="_blank"
-												rel="noreferrer noopener"
-											>
-												<Button
-													variant={
-														darkMode
-															? 'outline-light'
-															: 'outline-dark'
-													}
-													className="sbtn"
-												>
-													View Github Repo
-												</Button>
-											</a>
 										</div>
-										<br></br>
-									</Card>
-								</Col>
-								{/* <Col lg={4} sm={12}>
+									</center>
+									<br></br>
+									<div>
+										<a
+											href="https://clonefreshly.netlify.app/"
+											target="_blank"
+											rel="noreferrer noopener"
+										>
+											<Button
+												variant={
+													darkMode
+														? 'outline-light'
+														: 'outline-dark'
+												}
+												className="sbtn"
+											>
+												View Project
+											</Button>
+										</a>
+										<a
+											href="https://github.com/avinashkvar/freshly.git"
+											target="_blank"
+											rel="noreferrer noopener"
+										>
+											<Button
+												variant={
+													darkMode
+														? 'outline-light'
+														: 'outline-dark'
+												}
+												className="sbtn"
+											>
+												View Github Repo
+											</Button>
+										</a>
+									</div>
+									<br></br>
+								</Card>
+							</Col>
+							{/* <Col lg={4} sm={12}>
 								<Card
 									align="center"
 									className="exp"
@@ -207,92 +259,132 @@ const Experience = ({ darkMode }) => {
 									<br></br>
 								</Card>
 							</Col> */}
-								<Col lg={4} sm={12}>
-									<Card
-										align="center"
-										className="exp"
-										data-theme={darkMode ? 'dark' : 'light'}
-									>
-										<br></br>
-										<center>
-											<div class="flip-card">
-												<div class="flip-card-inner">
-													<div className="flip-card-front">
-														<Card.Img variant="top" src={ca} />
-														<Card.Body>
-															<Card.Title align="center">
-																BigBasket.com(Clone)
-															</Card.Title>
-															<Card.Text>
-																<p align="center">
-																	A Web Application where user
-																	can order ther Grocery
-																	products.
-																</p>
-															</Card.Text>
-														</Card.Body>
-													</div>
-													<div className="flip-card-back">
-														<p>
-															This is an Grocery Ordering Web
-															Application where the customers can
-															login and go through all the items
-															i.e
-															vegetables,fruits,staples,cosmetic
-															so on & then they can add the items
-															according to their cart and pay for
-															the above items via card or cash,
-															the items get delivered. Thank You
-															for ordering
-														</p>
-													</div>
+							<Col lg={4} sm={12}>
+								<Card
+									align="center"
+									className="exp"
+									data-theme={darkMode ? 'dark' : 'light'}
+								>
+									<br></br>
+									<center>
+										<div class="flip-card">
+											<div class="flip-card-inner">
+												<div className="flip-card-front">
+													<Card.Img variant="top" src={ca} />
+													<Card.Body>
+														<Card.Title align="center">
+															BigBasket.com(Clone)
+														</Card.Title>
+														<Card.Text>
+															<p align="center">
+																A Web Application where user can
+																order ther Grocery products.
+															</p>
+														</Card.Text>
+													</Card.Body>
+												</div>
+												<div className="flip-card-back">
+													<p>
+														This is an Grocery Ordering Web
+														Application where the customers can
+														login and go through all the items i.e
+														vegetables,fruits,staples,cosmetic so
+														on & then they can add the items
+														according to their cart and pay for
+														the above items via card or cash, the
+														items get delivered. Thank You for
+														ordering
+													</p>
+													<Button
+														variant={
+															darkMode
+																? 'outline-light'
+																: 'outline-dark'
+														}
+														className="skills-btn"
+													>
+														<FontAwesomeIcon
+															className="skill"
+															size="2x"
+															icon={faHtml5}
+														/>
+													</Button>
+													<Button
+														variant={
+															darkMode
+																? 'outline-light'
+																: 'outline-dark'
+														}
+														className="skills-btn"
+													>
+														<FontAwesomeIcon
+															className="skill"
+															size="2x"
+															icon={faCss3Alt}
+														/>
+													</Button>
+													<Button
+														variant={
+															darkMode
+																? 'outline-light'
+																: 'outline-dark'
+														}
+														className="skills-btn"
+													>
+														<FontAwesomeIcon
+															className="skill"
+															size="2x"
+															icon={faJs}
+														/>
+													</Button>
 												</div>
 											</div>
-										</center>
-										<br></br>
-										<div>
-											<a
-												href="https://bigbasket.vercel.app/"
-												target="_blank"
-												rel="noreferrer noopener"
-											>
-												<Button
-													variant={
-														darkMode
-															? 'outline-light'
-															: 'outline-dark'
-													}
-													className="sbtn"
-												>
-													View Project
-												</Button>
-											</a>
-											<a
-												href="https://github.com/avinashkvar/bigbasket.git"
-												target="_blank"
-												rel="noreferrer noopener"
-											>
-												<Button
-													variant={
-														darkMode
-															? 'outline-light'
-															: 'outline-dark'
-													}
-													className="sbtn"
-												>
-													View Github Repo
-												</Button>
-											</a>
 										</div>
-										<br></br>
-									</Card>
-								</Col>
-							</Row>
-						</Zoom>
-						<br></br>
-						<Zoom>
-							<Row>
-								{/* <Col lg={4} sm={12}>
+									</center>
+									<br></br>
+									<div>
+										<a
+											href="https://bigbasket.vercel.app/"
+											target="_blank"
+											rel="noreferrer noopener"
+										>
+											<Button
+												variant={
+													darkMode
+														? 'outline-light'
+														: 'outline-dark'
+												}
+												className="sbtn"
+											>
+												View Project
+											</Button>
+										</a>
+										<a
+											href="https://github.com/avinashkvar/bigbasket.git"
+											target="_blank"
+											rel="noreferrer noopener"
+										>
+											<Button
+												variant={
+													darkMode
+														? 'outline-light'
+														: 'outline-dark'
+												}
+												className="sbtn"
+											>
+												View Github Repo
+											</Button>
+										</a>
+									</div>
+									<br></br>
+								</Card>
+							</Col>
+						</Row>
+					</Zoom>
+					<br></br>
+					<Zoom>
+						<Row>
+							{/* <Col lg={4} sm={12}>
 								<Card
 									align="center"
 									className="exp"
@@ -442,7 +534,7 @@ const Experience = ({ darkMode }) => {
 									<br></br>
 								</Card>
 							</Col> */}
-								{/* <Col lg={4} sm={12}>
+							{/* <Col lg={4} sm={12}>
 								<Card
 									align="center"
 									className="exp"
@@ -503,8 +595,8 @@ const Experience = ({ darkMode }) => {
 									<br></br>
 								</Card>
 							</Col> */}
-							</Row>
-						</Zoom>
+						</Row>
+					</Zoom>
 				</Container>
 			</Fade>
 			<br></br>
